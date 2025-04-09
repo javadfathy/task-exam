588956ea4a0068b56666cb0cc67f7bafc4bdac78
javadfathy <jwadfthy2@gmail.com>
2025-04-09 19:32:29 +0330

    done the scenario 4

---
+++ scenario4/scenario4/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {RecycleScroller} from "vue-virtual-scroller";
+import {ref} from "vue";
+
+const users = ref(Array.from({length: 10000}, (_, i) => ({
+  id: i + 1,
+  name: `User ${i + 1}`,
+})))
+
+</script>
+
+<template>
+  <section>
+    <RecycleScroller
+        class="scroller"
+        :items="users"
+        :item-size="64"
+        key-field="id"
+        v-slot="{item}"
+        :buffer="200"
+        page-mode
+    >
+      <div class="user">
+        {{item.name}}
+      </div>
+    </RecycleScroller>
+  </section>
+</template>
---
+++ scenario4/scenario4/src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
